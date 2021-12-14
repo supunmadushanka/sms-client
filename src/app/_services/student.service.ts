f79c0e6b354464784325711a825415af5549da0f
supun <supunmadushanka19980822@gmail.com>
2021-12-14 21:13:04 +0530

    service created

---
+++ src/app/_services/student.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  public customError = {
+    status: 500,
+    message: 'Sorry! Something went wrong :('
+  }
+
+  private studentAdd: string = `${environment.baseURL}students/add`;
+  private familyGet: string = `${environment.baseURL}students/get`;
+
+  constructor(private httpClient: HttpClient) { }
+
+  public addStudent = (model): Observable<any> => {
+    return this.httpClient.post<any>(this.studentAdd, model).pipe(
+      catchError((err: HttpErrorResponse) => {
+        return throwError(err || this.customError);
+      })
+    );
+  }
+
+  public getFamilies = (hotelId: string): Observable<any> => {
+    return this.httpClient.get<any>(`${this.familyGet}/${hotelId}`).pipe(
+      catchError((err: HttpErrorResponse) => {
+        return throwError(err || this.customError);
+      })
+    );
+  }
+}
